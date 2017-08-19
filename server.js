8978035034e84cb8e296e817d68f3ce39aaf52f1
boopathy435 <kboopathyk7@gmail.com>
2017-08-19 14:18:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,11 +81,11 @@ pool.query("SELECT * FROM test WHERE title = "+req.params.mem, function(err, res
     if(err){
         res.status(500).send(err.toString());
     }else{
-        if(result.rows.length ===0){
+        if(result.rows.length === 0){
             res.status(404).send('Article not found');
         }else{
-            var mem=result.rows[0];
-            res.send(createtemplate(mem));
+            var meme=result.rows[0];
+            res.send(createtemplate(meme));
         }
     }
 });
